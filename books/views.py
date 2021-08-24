7994300d1489c8f8f1aeb77af987968f54305b79
salik <saliksheraz786@gmail.com>
2021-08-24 20:08:22 +0500

    changes for video

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from rest_framework.response import Response
 from django.core import serializers
 from rest_framework.permissions import IsAuthenticated
@@ -11,12 +12,13 @@ class volumeViewSet(APIView):
     def get(self, request):
         allData = Volume.objects.all()
         qs_json = serializers.serialize('json', allData)
-        return Response(qs_json)
+        return HttpResponse(qs_json, content_type='application/json')
 
     def post(self, request):
         name = request.POST.get('name')
         Volume.objects.create(name=name)
-        return Response(status=200)
+        content = {'message': 'Volume added successfully'}
+        return Response(content)
 
 
 class bookshelfViewSet(APIView):
@@ -25,18 +27,20 @@ class bookshelfViewSet(APIView):
     def get(self, request):
         allData = Bookshelf.objects.filter(user=request.user) | Bookshelf.objects.filter(is_private=False)
         qs_json = serializers.serialize('json', allData)
-        return Response(qs_json)
+        return HttpResponse(qs_json, content_type='application/json')
 
     def post(self, request):
         name = request.POST.get('name')
-        volume = request.POST.get('volume')
+        volume_id = request.POST.get('volume_id')
         is_private = request.POST.get('is_private')
         if is_private == 'true':
             is_private = True
         else:
             is_private = False
-        Bookshelf.objects.create(name=name, user=request.user, is_private=is_private, volume=int(volume))
-        return Response(status=200)
+        Bookshelf.objects.create(name=name, user=request.user, is_private=is_private,
+                                 volume=Volume.objects.get(id=volume_id))
+        content = {'message': 'Bookshelf added successfully'}
+        return Response(content)
 
 
 class readingPosViewSet(APIView):
@@ -45,13 +49,18 @@ class readingPosViewSet(APIView):
     def get(self, request):
         allData = ReadingPos.objects.filter(user=request.user)
         qs_json = serializers.serialize('json', allData)
-        return Response(qs_json)
+        return HttpResponse(qs_json, content_type='application/json')
 
     def post(self, request):
         val = request.POST.get('val')
-        volume = request.POST.get('volume')
-        ReadingPos.objects.create(val=val, user=request.user, volume=int(volume))
-        return Response(status=200)
+        volume_id = request.POST.get('volume_id')
+        dublicateCheck = ReadingPos.objects.filter(volume=Volume.objects.get(id=volume_id), user=request.user)
+        if dublicateCheck:
+            content = {'message': 'Reading Position to this volume is already given by this user'}
+            return Response(content)
+        ReadingPos.objects.create(val=int(val), user=request.user, volume=Volume.objects.get(id=volume_id))
+        content = {'message': 'Reading position added successfully'}
+        return Response(content)
 
 
 class reviewViewSet(APIView):
@@ -60,13 +69,15 @@ class reviewViewSet(APIView):
     def get(self, request):
         allData = Review.objects.filter(user=request.user)
         qs_json = serializers.serialize('json', allData)
-        return Response(qs_json)
+        return HttpResponse(qs_json, content_type='application/json')
 
     def post(self, request):
         val = request.POST.get('val')
-        volume = request.POST.get('volume')
-        dublicateCheck = Review.objects.filter(user=request.user, volume_id=int(volume))
+        volume_id = request.POST.get('volume_id')
+        dublicateCheck = Review.objects.filter(user=request.user, volume=Volume.objects.get(id=volume_id))
         if dublicateCheck:
-            return Response(status=404)
-        Review.objects.create(val=val, user=request.user, volume=int(volume))
-        return Response(status=200)
+            content = {'message': 'Review to this volume is already given by this user'}
+            return Response(content)
+        Review.objects.create(val=int(val), user=request.user, volume=Volume.objects.get(id=volume_id))
+        content = {'message': 'Review added successfully'}
+        return Response(content)
